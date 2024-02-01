60e559dbd8f900dc4a2b3516a391032f56bf9dc0
dedmaksim03 <sergeevih.08@mail.ru>
2024-02-01 17:05:40 +0300

    First commit

---
+++ src/main/java/com/example/hbcalendar/HbcalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.hbcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbcalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbcalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hbcalendar/HbcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbcalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
